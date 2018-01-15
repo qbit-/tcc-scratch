f99b1fc38f8fc1b1a7cdc63efbfdcfc2f5c271f2
Qbit- <rss3@rice.edu>
2018-01-14 20:23:42 -0600

    residuals calculation

--- systems/run_system.py
+++ systems/run_system.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3.6
+#!/home/ilias/.local/bin/python3.6
 """
 These scripts are for running molecular systems
 with RCCSD-CPD
@@ -78,7 +78,7 @@ def run_cc_ref(workdir):
     rhf.scf()
 
     # Run RCCSD_RI
-    from tcc.rccsd_mul import RCCSD_MUL_RI
+    from tcc.rccsd import RCCSD_DIR_RI
     from tcc.cc_solvers import residual_diis_solver
 
     tim = time.process_time()
@@ -86,7 +86,7 @@ def run_cc_ref(workdir):
     if (not isfile(workdir + 'ccsd_results.p')
             or not isfile(workdir + 'RCCSD.txt')):
 
-        cc = RCCSD_MUL_RI(rhf,
+        cc = RCCSD_DIR_RI(rhf,
                           mo_coeff=mo_coeff,
                           mo_energy=mo_energy)
 
@@ -124,24 +124,25 @@ def run_cc_cpd(workdir):
     mol.basis = BASIS
     mol.build()
 
-    nbasis = mol.nao_nr()
+    # nbasis = mol.nao_nr()
 
     rhf = scf.density_fit(scf.RHF(mol))
     rhf.max_cycle = 1
     rhf.scf()
 
+    nbasis_ri = rhf.with_df.get_naoaux()
     # Get CCSD results to generate initial guess
 
-    with open(workdir + 'ccsd_results.p', 'wb') as fp:
+    with open(workdir + 'ccsd_results.p', 'rb') as fp:
         energy_ref, amps_ref = pickle.load(fp)
 
     # Run RCCSD_RI_CPD
     from tcc.rccsd_cpd import RCCSD_nCPD_LS_T
-    from tcc.cc_solvers import classic_solver
+    from tcc.cc_solvers import classic_solver, update_diis_solver
     from tcc.tensors import Tensors
     from tcc.cpd import ncpd_initialize, als_dense
 
-    ranks_t = [el * nbasis for el in RANKS_T_FACTOR]
+    ranks_t = [int(el * nbasis_ri) for el in RANKS_T_FACTOR]
 
     energies = []
     deltas = []
@@ -156,19 +157,21 @@ def run_cc_cpd(workdir):
                                  mo_energy=mo_energy)
             # Initial guess
             t2x = als_dense(
-                ncpd_initialize(amps_ref.shape, rank),
-                amps_ref, max_cycle=100, tensor_format='ncpd'
+                ncpd_initialize(amps_ref.t2.shape, rank),
+                amps_ref.t2, max_cycle=100, tensor_format='ncpd'
             )
             t2names = ['xlam', 'x1', 'x2', 'x3', 'x4']
             amps_guess = Tensors(t1=amps_ref.t1,
                                  t2=Tensors(zip(t2names, t2x)))
 
-            converged, energy, amps = classic_solver(
-                cc, conv_tol_energy=1e-9, conv_tol_res=1e-8,
+            converged, energy, amps = update_diis_solver(
+                cc, conv_tol_energy=1e-6, conv_tol_res=1e-6,
                 max_cycle=500,
                 verbose=logger.INFO,
-                amps=amps_guess)
+                amps=amps_guess, ndiis=3)
 
+            if not converged:
+                energy = np.nan
             ccsd_results = [energy, amps]
             with open(workdir +
                       'ccsd_results_rank_{}.p'.format(rank), 'wb') as fp:
@@ -189,9 +192,94 @@ def run_cc_cpd(workdir):
 
     np.savetxt(
         workdir + 'RCCSD_CPD.txt',
-        ranks_t, energies, deltas, header='Rank Energy Delta',
-        fmt=('%i', '%e', '%e',)
+        np.column_stack((ranks_t, energies, deltas)),
+        header='Rank Energy Delta',
+        fmt=('%i', '%e', '%e')
     )
+    print('Summary')
+    print('=========================================')
+    for idxr, rank in enumerate(ranks_t):
+        print('{} {}'.format(rank, deltas[idxr]))
+    print('=========================================')
+
+
+def calculate_residuals(workdir):
+    print('Running CC-CPD')
+
+    # Restore RHF object
+    with open(workdir + 'rhf_results.p', 'rb') as fp:
+        rhf_results = pickle.load(fp)
+
+    e_tot, mo_coeff, mo_energy, atom = rhf_results
+    mol = gto.Mole()
+    mol.atom = atom
+    mol.basis = BASIS
+    mol.build()
+
+    # nbasis = mol.nao_nr()
+
+    rhf = scf.density_fit(scf.RHF(mol))
+    rhf.max_cycle = 1
+    rhf.scf()
+
+    nbasis_ri = rhf.with_df.get_naoaux()
+
+    # Load RCCSD_RI_CPD results
+    from tcc.rccsd_cpd import RCCSD_nCPD_LS_T
+    from tcc.cc_solvers import classic_solver, update_diis_solver
+    from tcc.tensors import Tensors
+    from tcc.cpd import ncpd_rebuild
+
+    ranks_t = [int(el * nbasis_ri) for el in RANKS_T_FACTOR]
+
+    norms_t1 = []
+    norms_t2 = []
+
+    for idxr, rank in enumerate(ranks_t):
+        tim = time.process_time()
+        if isfile(
+                workdir +
+                'ccsd_results_rank_{}.p'.format(rank)):
+            with open(workdir +
+                      'ccsd_results_rank_{}.p'.format(rank), 'rb') as fp:
+                ccsd_results = pickle.load(fp)
+                energy, amps = ccsd_results
+
+            # Create cc object
+            cc = RCCSD_nCPD_LS_T(rhf,
+                                 mo_coeff=mo_coeff,
+                                 mo_energy=mo_energy)
+            ham = cc.create_ham()
+            res = cc.calc_residuals(ham, amps)
+
+            # residuals
+            res_full = Tensors(
+                t1=res.t1,
+                t2=ncpd_rebuild([res.t2.xlam, res.t2.x1, res.t2.x2,
+                                 res.t2.x3, res.t2.x4])
+            )
+
+            norms = res_full.map(np.linalg.norm)
+
+        else:
+            raise FileNotFoundError(
+                workdir +
+                'ccsd_results_rank_{}.p does not exist'.format(rank))
+
+        norms_t1.append(norms.t1)
+        norms_t2.append(norms.t2)
+
+    np.savetxt(
+        workdir + 'RCCSD_CPD_RES.txt',
+        np.column_stack((ranks_t, norms_t1, norms_t2)),
+        header='Rank |R1| |R2|',
+        fmt=('%i', '%e', '%e')
+    )
+    print('Summary')
+    print('=========================================')
+    for idxr, rank in enumerate(ranks_t):
+        print('{} {}'.format(rank, norms_t2[idxr]))
+    print('=========================================')
 
 
 def collect_table():
@@ -226,15 +314,36 @@ def collect_table():
                 + '\n')
 
 
-if __name__ == '__main__':
-    if (len(sys.argv) != 2 or not isdir(sys.argv[1])):
-        raise ValueError('usage: run_system.py folder')
-    wd = sys.argv[1]
-    wd = wd.rstrip('/') + '/'  # Make sure we have the workdir
-    # with the trailing slash
+def run_all():
+    """
+    Run all systems until we fail!
+    """
+    contents = os.listdir()
+    dirs = [elem for elem in sorted(contents) if isdir(elem)]
+
+    for dirname in dirs:
+        print('Working on: {}'.format(dirname))
+        run_dir(dirname + '/')
+
+    collect_table()
+
+
+def run_dir(wd):
 
     # Run RHF
     build_rhf(wd)
 
     # Run RCCSD
     run_cc_ref(wd)
+
+    # Run RCCSD-CPD
+    run_cc_cpd(wd)
+
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 2 or not isdir(sys.argv[1])):
+        raise ValueError('usage: run_system.py folder')
+    wd = sys.argv[1]
+    wd = wd.rstrip('/') + '/'  # Make sure we have the workdir
+    # with the trailing slash
+    run_dir(wd)
